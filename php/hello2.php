c2729740cfaf53dd71f94d9dc66907b819128848
IWAMI Takehiro <sheepsan@gmail.com>
2018-02-01 17:49:41 +0900

    テスト用

---
+++ php/hello2.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: primera
+ * Date: 2018/02/01
+ * Time: 16:20
+ */
+
+Class Hello{
+    //メンバ変数
+    private $message=null;
+
+    public function __construct()
+    {
+        $this->message="Hello";
+    }
+
+    public function say(){
+        echo $this->message;
+    }
+}
+
+$obj=new Hello();
+$obj->say();
